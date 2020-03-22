0f913fc563920816516f6b6ce3a33032708b70ed
Phil413 <845552092@qq.com>
2020-03-22 22:27:09 +0800

    leetcode 101 迭代

--- python/101_SymmetricTree.py
+++ python/101_SymmetricTree.py
@@ -8,6 +8,7 @@
 class Solution(object):
     def isSymmetric(self, root):
         """
+        递归
         :type root: TreeNode
         :rtype: bool
         """
@@ -25,3 +26,27 @@ class Solution(object):
                 return False
         else:
             return False
+
+    def isSymmetric2(self, root):
+        """
+        迭代
+        :type root: TreeNode
+        :rtype: bool
+        """
+        tmp_list = [root, root]
+        p = root
+        while (tmp_list):
+            if len(tmp_list) < 2:
+                return False
+            left = tmp_list.pop(0)
+            right = tmp_list.pop(0)
+            if not left and not right:
+                continue
+            if not left or not right:
+                return False
+            if left.val == right.val:
+                tmp_list.extend([left.left, right.right])
+                tmp_list.extend([left.right, right.left])
+            else:
+                return False
+        return True
